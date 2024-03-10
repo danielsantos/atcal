ecba1271e07ae362bdde8d26434611f4240a617b
Daniel dos Santos Rocha <daniel.arocha@mercadolivre.com>
2024-03-10 19:37:52 -0300

    log user e ip

--- src/main/java/com/teoali/atcal/controller/ClientController.java
+++ src/main/java/com/teoali/atcal/controller/ClientController.java
@@ -1,12 +1,15 @@
 package com.teoali.atcal.controller;
 
+import com.teoali.atcal.config.MyUserPrincipal;
 import com.teoali.atcal.domain.Client;
 import com.teoali.atcal.domain.Payment;
+import com.teoali.atcal.domain.User;
 import com.teoali.atcal.domain.enums.Status;
 import com.teoali.atcal.repository.ClientRepository;
 import com.teoali.atcal.repository.GroupRepository;
 import com.teoali.atcal.repository.PaymentRepository;
 import com.teoali.atcal.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,10 +22,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("/clients")
 public class ClientController {
+
+  Logger logger = (Logger) LoggerFactory.getLogger(ClientController.class);
 
   @Autowired
   private ClientRepository clientRepository;
@@ -37,7 +44,9 @@ public class ClientController {
   private UserService userService;
 
   @GetMapping
-  public String list(Model model, Authentication authentication) {
+  public String list(Model model, Authentication authentication, HttpServletRequest request) {
+    User user = ((MyUserPrincipal) authentication.getPrincipal()).getUser();
+    logger.info("audit - usuario: " + user.getUsername() + " - ip: " + request.getRemoteAddr());
     model.addAttribute("clients", clientRepository.findByUser(userService.getUser(authentication)));
     return "clients/list";
   }
